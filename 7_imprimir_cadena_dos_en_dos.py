4758336999ff6145f0e7612ac82bb7bd6c8271c6
Heidy Diaz <heidydiazbarrios@gmail.com>
2025-11-02 20:01:56 +0100

    7 dividir cadena de dos en dos

---
+++ 7_imprimir_cadena_dos_en_dos.py
@@ -0,0 +1,23 @@
+#Complete the solution so 
+# that it splits the string into pairs of two characters. 
+# If the string contains an odd number of characters then it 
+# should replace the missing second character of the final pair 
+# with an underscore ('_').
+
+#Examples:
+
+# 'abc' =>  ['ab', 'c_']
+# 'abcdef' => ['ab', 'cd', 'ef']
+
+
+def solution(s):
+    if len(s)%2!=0:
+        s= s + "_"  # si es impar el str agregar _
+    nuevo_arr = []
+    for i in range(0, len(s), 2):
+        nuevo_arr.append(str(s[i]+s[i+1]))
+        
+    return nuevo_arr
+
+print (solution('abc'))
+print (solution('abcddfghgdedd'))
