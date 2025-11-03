c4c99f6be2a8f9702aaeccbbd9101bf2d05bf844
Heidy Diaz <heidydiazbarrios@gmail.com>
2025-11-03 21:43:21 +0100

    10 con cometarios

--- 10_unir_cadenas_y_sorted.py
+++ 10_unir_cadenas_y_sorted.py
@@ -1,8 +1,8 @@
 def longest(a1, a2):
     combined = a1 + a2
-    unique_chars = set(combined)
-    sorted_chars = sorted(unique_chars)
-    result = ''.join(sorted_chars)
+    unique_chars = set(combined) # eliminar duplicados
+    sorted_chars = sorted(unique_chars) @# ordenar caracteres
+    result = ''.join(sorted_chars)# unir en una cadena
     return result
 
 print (longest("aretheyhere", "yestheyarehere"))
