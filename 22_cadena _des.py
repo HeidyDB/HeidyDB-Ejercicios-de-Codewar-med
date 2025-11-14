479c18799c3fcd4b5499ad6bb290b98691d89592
Heidy Diaz <heidydiazbarrios@gmail.com>
2025-11-14 21:56:01 +0100

    22

---
+++ 22_cadena _des.py
@@ -0,0 +1,7 @@
+
+def pig_it(text):
+    return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
+
+print(pig_it('Hello world !'))  # Imprime: 'elloHay orldway !'
+print(pig_it('Pig latin is cool'))  # Imprime: 'igPay atinlay siay oolcay'
+print(pig_it('This is my string'))  # Imprime: 'hisTay siay ymay tringsay'
