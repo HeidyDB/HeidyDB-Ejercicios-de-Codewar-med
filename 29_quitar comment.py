cb63aa78b2bd4e6124b3918f9fd92d60ad9a6b44
Heidy Diaz <heidydiazbarrios@gmail.com>
2025-11-25 21:47:52 +0100

    29 comments

--- 29_quitar comment.py
+++ 29_quitar comment.py
@@ -6,8 +6,9 @@ def strip_comments(strng, markers):
     for line in lines:
         for marker in markers:
             if marker in line:
-                line = line.split(marker)[0]
-        result_lines.append(line.rstrip())
+                line = line.split(marker)[0] # keep part before marker
+        result_lines.append(line.rstrip()) # remove trailing whitespace
     
     return '\n'.join(result_lines)
 print(strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
+# La función strip_comments elimina los comentarios de un string dado. 
